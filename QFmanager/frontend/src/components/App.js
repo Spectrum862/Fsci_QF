971a46c904628e71fb3883be4904ab317b8d2cc2
WayneSkywalker <GrandCharge@outlook.com>
2020-01-19 18:01:51 +0700

    login and main pages progress

--- QFmanager/frontend/src/components/App.js
+++ QFmanager/frontend/src/components/App.js
@@ -38,16 +38,56 @@ class App extends React.Component {
 */
 
 /*
-<img id="qffulllogo" src="..\static\QFLOGO-1.png"></img>
 <div className="login">
+    <img id="qffulllogo" src="..\static\QFLOGO-1.png"></img>
     <form>
-        <input type="text" name="username" placeholder="USERNAME" maxlength="11"></input>
+        <input type="text" name="username" placeholder="USERNAME" maxLength="20"></input>
         <br></br>
         <input type="password" name="password" placeholder="PASSWORD"></input>
         <br></br>
         <button type="submit" id="loginbtn"><strong>LOG IN</strong></button>
     </form>
-    <p>HAVE A PROBLEM? CONTACT US.</p>
+    HAVE A PROBLEM? <a className="contactlink" href="">CONTACT US</a>.
+</div>
+*/
+
+/*
+<div className="qfnavbar">
+    <div className="row col-12">
+        <div className="col-4">
+            <a href="#"><img id="qflogo" src="..\static\QFLOGO-0.png"></img></a>
+        </div>
+        <div className="col-4">
+            <form>
+                <input type="text" name="search" placeholder="Search an activity"></input>
+            </form>
+        </div>
+        <div className="col-2">
+            <a href="#"><p>Firstname Lastname</p></a>
+        </div>
+        <div className="col-1">
+            <a id="logout" href="#"><p>log out</p></a>
+        </div>
+    </div>
+</div>
+<div className="menu col-4">
+    <img id="qffulllogo" src="..\static\QFLOGO-1.png"></img>
+    <p className="forwho">for the <b>ADVISOR</b></p>
+    <a href="#">
+        <div className="menubar">
+            <p>Personal Student's QF Profile</p>
+        </div>
+    </a>
+    <a href="#">
+        <div className="menubar">
+            <p>Evaluate an Activity</p>
+        </div>
+    </a>
+    <a href="#">
+        <div className="menubar">
+            <p>QF Overview</p>
+        </div>
+    </a>
 </div>
 */
 
