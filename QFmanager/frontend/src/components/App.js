e95b1fc6233a3cf96b9e2fc5a56644b4ea55bf33
WayneSkywalker <GrandCharge@outlook.com>
2020-01-20 22:07:14 +0700

    edit .CSS #2

--- QFmanager/frontend/src/components/App.js
+++ QFmanager/frontend/src/components/App.js
@@ -11,27 +11,6 @@ class App extends React.Component {
     render() {
         return (
             <Fragment>
-                <div className="qfnavbar">
-                    <div className="row col-12">
-                        <div className="col-1"></div>
-                        <div className="col-2">
-                            <a href="#"><img id="qflogo" src="..\static\QFLOGO-0.png"></img></a>
-                        </div>
-                        <div className="col-1"></div>
-                        <div className="col-4">
-                            <form>
-                                <input type="text" name="search" placeholder="Search an activity"></input>
-                            </form>
-                        </div>
-                        <div className="col-1"></div>
-                        <div className="col-05"></div>
-                        <div className="col-1">
-                            <a id="logout" href="#"><p>log out</p></a>
-                        </div>
-                        <div className="col-05"></div>
-                        <div className="col-1"></div>
-                    </div>
-                </div>
                 
             </Fragment>
         );
@@ -82,25 +61,6 @@ class App extends React.Component {
         <div className="col-05"></div>
         <div className="col-1"></div>
     </div>
-</div>
-<div className="menu col-4">
-    <img id="qffulllogo" src="..\static\QFLOGO-1.png"></img>
-    <p className="forwho">for the <b>ADVISOR</b></p>
-    <a href="#">
-        <div className="menubar">
-            <p>Personal Student's QF Profile</p>
-        </div>
-    </a>
-    <a href="#">
-        <div className="menubar">
-            <p>Evaluate an Activity</p>
-        </div>
-    </a>
-    <a href="#">
-        <div className="menubar">
-            <p>QF Overview</p>
-        </div>
-    </a>
 </div>
 */
 
