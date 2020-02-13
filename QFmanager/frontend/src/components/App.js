48879c648e3da0b4cba2121cfbb82663182f01fc
WayneSkywalker <GrandCharge@outlook.com>
2020-02-13 11:15:53 +0700

    edit .CSS #5

--- QFmanager/frontend/src/components/App.js
+++ QFmanager/frontend/src/components/App.js
@@ -11,22 +11,14 @@ class App extends React.Component {
     render() {
         return (
             <Fragment>
-                <div className="qfheader">
-                    <div className="row col-12">
-                        <div className="col-1">
-                            <img className="profilepic" src="..\static\IMG_8139.jpg"></img>
-                        </div>
-                    </div>
-                </div>
-                <div className="qfnavbar fixed-bottom">
-                    <div className="row">
-                        <div className="col-6">
-                            <a className="qfnavlink" href="#"><img className="icon_home" src="..\static\homeactive.svg"></img><br></br>หน้าแรก</a>
-                        </div>
-                        <div className="col-6">
-                            <a className="qfnavlink" href="#"><img className="icon_s_activity" src="..\static\QFactive_.svg"></img><br></br>ค้นหากิจกรรม</a>
-                        </div>
-                    </div>
+                <div className="login">
+                    <img id="qffulllogo" src="..\static\QFLOGO-1.png"></img>
+                    <form>
+                        <input type="text" name="username" placeholder="USERNAME" maxLength="20"></input>
+                        <input type="password" name="password" placeholder="PASSWORD"></input>
+                        <button type="submit" id="loginbtn"><strong>LOG IN</strong></button>
+                    </form>
+                    HAVE A PROBLEM? <a className="contactlink" href="">CONTACT US</a>.
                 </div>
             </Fragment>
         );
